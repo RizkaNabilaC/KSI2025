96e13170416e1e9ec8074daab40d1b01fe825f5f
RizkaNabilaC <nabilarizka815@gmail.com>
2025-11-07 19:20:16 +0700

    Menambahkan index.html dan index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Halaman Utama</title>
+</head>
+<body>
+  <h1>Selamat Datang di Aplikasi Tambah Data</h1>
+  <a href="tambah.php">Tambah Data Baru</a>
+</body>
+</html>
